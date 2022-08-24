3af59cb612f5d4766cb179edefb4309309932f21
vidigoz <diazg88@gmail.com>
2022-08-23 21:15:32 -0700

    elimine no owner

--- contracts/wedding.sol
+++ contracts/wedding.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.16;
 //import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 //import "./PriceConverter.sol";
 
-error NotOwner();
+
 
 contract mainMatrimonio{
     struct persona {
